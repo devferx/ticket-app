c1eefe44543e6b41af5a1d11ab0c96c4334aeae1
devferx <ferkki@hotmail.com>
2022-04-30 21:30:47 -0400

    Router entre pantallas

--- src/pages/RouterPage.tsx
+++ src/pages/RouterPage.tsx
@@ -1,52 +1,66 @@
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+
 import { Layout, Menu } from "antd";
 import {
   UserOutlined,
   VideoCameraOutlined,
   UploadOutlined,
 } from "@ant-design/icons";
+import { Login } from "./Login";
+import { Queue } from "./Queue";
+import { CreateTicket } from "./CreateTicket";
+import { Desktop } from "./Desktop";
 
-const { Header, Sider, Content } = Layout;
+const { Sider, Content } = Layout;
 
 export const RouterPage = () => {
   return (
-    <Layout style={{ height: "100vh" }}>
-      <Sider>
-        <div className="logo" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["1"]}
-          items={[
-            {
-              key: "1",
-              icon: <UserOutlined />,
-              label: "Login",
-            },
-            {
-              key: "2",
-              icon: <VideoCameraOutlined />,
-              label: "Queue",
-            },
-            {
-              key: "3",
-              icon: <UploadOutlined />,
-              label: "Create Ticket",
-            },
-          ]}
-        />
-      </Sider>
-      <Layout className="site-layout">
-        <Content
-          className="site-layout-background"
-          style={{
-            margin: "24px 16px",
-            padding: 24,
-            minHeight: 280,
-          }}
-        >
-          Content
-        </Content>
+    <Router>
+      <Layout style={{ height: "100vh" }}>
+        <Sider hidden={false}>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={["1"]}
+            items={[
+              {
+                key: "1",
+                icon: <UserOutlined />,
+                label: <Link to="/login">Login</Link>,
+              },
+              {
+                key: "2",
+                icon: <VideoCameraOutlined />,
+                label: <Link to="/queue">Queue</Link>,
+              },
+              {
+                key: "3",
+                icon: <UploadOutlined />,
+                label: <Link to="/create-ticket">Create Ticket</Link>,
+              },
+            ]}
+          />
+        </Sider>
+        <Layout className="site-layout">
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: "24px 16px",
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/queue" element={<Queue />} />
+              <Route path="/create-ticket" element={<CreateTicket />} />
+              <Route path="/desktop" element={<Desktop />} />
+              <Route path="/*" element={<Login />} />
+            </Routes>
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </Router>
   );
 };
