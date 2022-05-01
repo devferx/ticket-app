c66c3ce8a2530fe478c5b2358cd122a2b46bc426
devferx <ferkki@hotmail.com>
2022-05-01 11:38:53 -0400

    Pantalla para crear un ticket

--- src/pages/CreateTicket.tsx
+++ src/pages/CreateTicket.tsx
@@ -1,3 +1,39 @@
+import { DownloadOutlined } from "@ant-design/icons";
+import { Col, Row, Typography, Button } from "antd";
+const { Title, Text } = Typography;
+
 export const CreateTicket = () => {
-  return <div>CreateTicket</div>;
+  const generateTicket = () => {};
+
+  return (
+    <>
+      <Row>
+        <Col className="center-content-col" span={14} offset={6}>
+          <Title style={{ textAlign: "center" }}>
+            Click the button to create a ticket
+          </Title>
+
+          <Button
+            type="primary"
+            shape="round"
+            size="large"
+            onClick={generateTicket}
+            icon={<DownloadOutlined />}
+          >
+            New Ticket
+          </Button>
+        </Col>
+      </Row>
+
+      <Row style={{ marginTop: 100 }}>
+        <Col className="center-content-col" span={14} offset={6}>
+          <Text>Your number</Text>
+          <br />
+          <Text type="success" style={{ fontSize: 55 }}>
+            55
+          </Text>
+        </Col>
+      </Row>
+    </>
+  );
 };
